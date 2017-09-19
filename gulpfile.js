d8f1d408c88459c3225c4766bf2d31341ef5f49e
Arif Shanji <arif@kano.me>
2017-09-19 15:55:29 +0100

    Move gtm snippet to head

--- gulpfile.js
+++ gulpfile.js
@@ -138,8 +138,8 @@ gulp.task('compress', () => {
             minifyCSS: true,
             removeComments: true
         })))
-        .pipe($.if(isIndexHtml, $.replace('</body></html>', `<noscript><iframe src='//www.googletagmanager.com/ns.html?id=GTM-WMGKFR' height='0' width='0' style='display: none; visibility: hidden;'></iframe></noscript>
-            <script>window.addEventListener('load',function(){(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WMGKFR')});</script></body></html>`)))
+        .pipe($.if(isIndexHtml, $.replace('</head>', `<noscript><iframe src='//www.googletagmanager.com/ns.html?id=GTM-WMGKFR' height='0' width='0' style='display: none; visibility: hidden;'></iframe></noscript>
+            <script>window.addEventListener('load',function(){(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WMGKFR')});</script></head>`)))
         .pipe($.if(hasExt('css'), $.cssSlam()))
         .pipe($.if(hasExt('js'), $.babel({ presets: [['es2015', { modules: false }]]})))
         .pipe($.if(hasExt('js'), $.uglify()))
